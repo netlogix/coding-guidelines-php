3c20cb6fb1f644531b9bb67be8e383f7e04b1143
Lars Lauger <lars.lauger@netlogix.de>
2022-09-19 22:50:42 +0200

    FEATURE: Add getters for configuration

--- src/DefaultPhp.php
+++ src/DefaultPhp.php
@@ -1,4 +1,5 @@
 <?php
+
 declare(strict_types=1);
 
 namespace Netlogix\CodingGuidelines\Php;
@@ -9,16 +10,26 @@ use Symplify\EasyCodingStandard\ValueObject\Set\SetList;
 
 final class DefaultPhp
 {
-
     public function configure(ECSConfig $ecsConfig): ECSConfig
     {
-        $ecsConfig->skip([
-            Fixer\Operator\NotOperatorWithSuccessorSpaceFixer::class,
-        ]);
-
         $ecsConfig->lineEnding("\n");
+        $ecsConfig->skip(DefaultPhp::getSkips());
+        $ecsConfig->sets(DefaultPhp::getSets());
+        $ecsConfig->rules(DefaultPhp::getRules());
 
-        $ecsConfig->sets([
+        return $ecsConfig;
+    }
+
+    public static function getSkips(): array
+    {
+        return [
+            Fixer\Operator\NotOperatorWithSuccessorSpaceFixer::class,
+        ];
+    }
+
+    public static function getSets(): array
+    {
+        return [
             SetList::PSR_12,
             SetList::CLEAN_CODE,
             SetList::DOCTRINE_ANNOTATIONS,
@@ -26,15 +37,17 @@ final class DefaultPhp
             SetList::ARRAY,
             SetList::DOCBLOCK,
             SetList::SYMPLIFY,
-        ]);
-
-        $ecsConfig->rule(Fixer\Whitespace\BlankLineBeforeStatementFixer::class);
-        $ecsConfig->rule(Fixer\Strict\DeclareStrictTypesFixer::class);
-        $ecsConfig->rule(Fixer\Strict\StrictComparisonFixer::class);
-        $ecsConfig->rule(Fixer\Strict\StrictParamFixer::class);
-        $ecsConfig->rule(Fixer\Import\GlobalNamespaceImportFixer::class);
-
-        return $ecsConfig;
+        ];
     }
 
+    public static function getRules(): array
+    {
+        return [
+            Fixer\Whitespace\BlankLineBeforeStatementFixer::class,
+            Fixer\Strict\DeclareStrictTypesFixer::class,
+            Fixer\Strict\StrictComparisonFixer::class,
+            Fixer\Strict\StrictParamFixer::class,
+            Fixer\Import\GlobalNamespaceImportFixer::class,
+        ];
+    }
 }
